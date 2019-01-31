edefe9998c0ee14ba3bb0928f90164dc389f10eb
Zakria Kharsah <2140010037@iau.edu.sa>
2019-01-31 22:03:06 +0300

    Update passport.py

--- config/passport.py
+++ config/passport.py
@@ -6,8 +6,8 @@ SOCIAL_LOGIN = {
     'google': {
         'class_': oauth2.Google,
 
-        'consumer_key': '923896266353-pi3m0bmf0ina2eeke3ftvo0oj8fmf27i.apps.googleusercontent.com',
-        'consumer_secret': '6duG-Mm70NdMJp7YU5XwkMQv',
+        'consumer_key': '923896266353-tse696iqi8qgkajjq5ue858316m03ht6.apps.googleusercontent.com',
+        'consumer_secret': 'W-kGDtJqCX_K3IaXRpH9KW4B',
 
         'scope': oauth2.Google.user_info_scope,
         'url': 'https://www.googleapis.com/userinfo/v2/me',
